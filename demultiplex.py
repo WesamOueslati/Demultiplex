cea1d79db7ea0c7b2b88f4ca65e13cd512d88bfe
WesamOueslati <oueslatiwesam101@gmail.com>
2026-08-04 16:14:44 -0700

    updated main function

--- demultiplex.py
+++ demultiplex.py
@@ -2,6 +2,7 @@
 
 import bioinfo
 import gzip
+import json
 
 def reverse_complement(seq: str):
     tr_table = str.maketrans('ATCGN', 'TAGCN')
@@ -15,17 +16,17 @@ def is_high_qual(qual_seq: str ,seq , thresh: float):
 def is_valid(barcode, barcodes):
     return (barcode in barcodes.values())
 
-def open_files(barcode_pairs) -> dict:
+def open_files(barcode_pairs: list[str]) -> dict:
     handles = {}
     
     for pair in barcode_pairs:
-        handles[f"{pair}_r1"] = open(f"{pair}_r1.fq" ,'a')
-        handles[f"{pair}_r2"] = open(f"{pair}_r2.fq" ,'a')
+        handles[f"{pair}_r1"] = open(f"output/{pair}_r1.fq" ,'a')
+        handles[f"{pair}_r2"] = open(f"output/{pair}_r2.fq" ,'a')
         
-    handles[f"unknown_r1"] = open(f"unknown_r1.fq", 'a')
-    handles[f"unknown_r2"] = open(f"unknown_r2.fq", 'a')
-    handles[f"hopped_r1"] = open(f"hopped_r1.fq", 'a')
-    handles[f"hopped_r2"] = open(f"hopped_r2.fq", 'a')
+    handles[f"unknown_r1"] = open(f"output/unknown_r1.fq", 'a')
+    handles[f"unknown_r2"] = open(f"output/unknown_r2.fq", 'a')
+    handles[f"hopped_r1"] = open(f"output/hopped_r1.fq", 'a')
+    handles[f"hopped_r2"] = open(f"output/hopped_r2.fq", 'a')
 
     return handles
 
@@ -60,19 +61,23 @@ def store_barcode_pairs(barcodes, output):
 
     counts = {}
     
-    prod = itertools.product(barcodes_ls,barcodes_ls)
+    files = []
+
+    prod = itertools.product(barcodes_ls, repeat=2)
     with open(output, 'w') as out:
         for pair in prod:
+            if pair[0] == pair[1]:
+                files.append(f"{pair[0]}_{pair[1]}" )
             pair_str = f"{pair[0]}_{pair[1]}" 
             out.write(pair_str + "\n")
             counts[pair_str] = 0
-    return counts
+    return counts, files
     
 
-def demultiplex(R1: str, R2: str, R3: str, R4: str, barcodes: dict, counts: dict) -> dict:
-    
-    handles = open_files(counts.keys())
-    with open(R1, 'r') as r1, open(R2, 'r') as r2, open(R3, 'r') as r3, open(R4, 'r') as r4:
+def demultiplex(R1: str, R2: str, R3: str, R4: str, barcodes: dict, output) -> dict:
+    counts, files = store_barcode_pairs(barcodes, output)
+    handles = open_files(files)
+    with gzip.open(R1, 'rt') as r1, gzip.open(R2, 'rt') as r2, gzip.open(R3, 'rt') as r3, gzip.open(R4, 'rt') as r4:
         count_matches = 0
         count_unknown = 0
         count_hopped = 0
@@ -97,34 +102,37 @@ def demultiplex(R1: str, R2: str, R3: str, R4: str, barcodes: dict, counts: dict
                 idx_1, idx_2 = r2_rec[1], r3_rec[1]
                 qual_1, qual_2 = r2_rec[3], r3_rec[3] 
                 
-                idx_str = f"{idx_1}_{idx_2}"
+                idx_str = f"{idx_1}_{reverse_complement(idx_2)}"
                 
                 rv = (idx_1 == reverse_complement(idx_2))
-                low_qual = not (is_high_qual(qual_1, seq_1, 5.0) and is_high_qual(qual_2, seq_2, 5.0))
-                valid = (is_valid(idx_1, barcodes) and is_valid(idx_2, barcodes))
+                # print(f"is reversed?: {rv}")
+                low_qual = not (is_high_qual(qual_1, seq_1, 30.0) and is_high_qual(qual_2, seq_2, 30.0))
+                # print(f"is low_quality?: {low_qual}")
+                valid = (is_valid(idx_1, barcodes) and is_valid(reverse_complement(idx_2), barcodes))
+                # print(f"is valid?: {valid}")
                 
                 if not valid or low_qual:
                     fh_r1 = handles[f"unknown_r1"]
                     fh_r2 = handles[f"unknown_r2"]
                     
-                    write_record(fh_r1, r1_rec, idx_1, idx_2)
-                    write_record(fh_r2, r4_rec, idx_1, idx_2)
+                    write_record(fh_r1, r1_rec, idx_str)
+                    write_record(fh_r2, r4_rec, idx_str)
                     count_unknown += 1
                 else:
                     if rv:
                         fh_r1 = handles[f"{idx_str}_r1"]
                         fh_r2 = handles[f"{idx_str}_r2"]
                         
-                        write_record(fh_r1, r1_rec, idx_1, idx_2)
-                        write_record(fh_r2, r4_rec, idx_1, idx_2)
+                        write_record(fh_r1, r1_rec, idx_str)
+                        write_record(fh_r2, r4_rec, idx_str)
                         counts[idx_str] += 1
                         count_matches += 1
                     else:
                         fh_r1 = handles["hopped_r1"]
                         fh_r2 = handles["hopped_r2"]
                         
-                        write_record(fh_r1, r1_rec, idx_1, idx_2)
-                        write_record(fh_r2, r4_rec, idx_1, idx_2)
+                        write_record(fh_r1, r1_rec, idx_str)
+                        write_record(fh_r2, r4_rec, idx_str)
                         counts[idx_str] += 1
                         count_hopped += 1
                 
@@ -132,7 +140,8 @@ def demultiplex(R1: str, R2: str, R3: str, R4: str, barcodes: dict, counts: dict
                 r2_rec.clear()
                 r3_rec.clear()
                 r4_rec.clear()
-
+                
+    close_files(handles)
     return counts
 if __name__ == "__main__":
     
@@ -141,26 +150,31 @@ if __name__ == "__main__":
     assert is_high_qual("IIII", "AAAN", 5.0) == False
     assert is_high_qual("IIII", "AAAA", 5.0) == True
     
-    # BARCODES_PATH="/projects/bgmp/shared/2017_sequencing/indexes.txt"
-    # BARCODE_PAIRS_PATH="/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/barcode_pairs.txt"
-    
-    # R1 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz"
-    # R2 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz"
-    # R3 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz"
-    # R4 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz"
-    
-    
-    BARCODES_PATH = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/barcodes.txt"
+    BARCODES_PATH="/projects/bgmp/shared/2017_sequencing/indexes.txt"
     BARCODE_PAIRS_PATH="/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/barcode_pairs.txt"
     
-    r1 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r1.fq"
-    r2 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r2.fq"
-    r3 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r3.fq"
-    r4 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r4.fq"
+    R1 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz"
+    R2 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz"
+    R3 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz"
+    R4 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz"
     
-    barcodes = barcodes_to_dict(BARCODES_PATH, sep=' ')
-    counts = store_barcode_pairs(barcodes, output=BARCODE_PAIRS_PATH)
     
-    demultiplex(r1, r2, r3, r4, barcodes, counts)
+    # BARCODES_PATH = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/barcodes.txt"
+    # BARCODE_PAIRS_PATH="/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/barcode_pairs.txt"
     
+    # r1 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r1.fq"
+    # r2 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r2.fq"
+    # r3 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r3.fq"
+    # r4 = "/projects/bgmp/oueslati/bioinfo/Bi622/AS1/Demultiplex/TEST-input_FASTQ/r4.fq"
+    
+    # barcodes = barcodes_to_dict(BARCODES_PATH, sep=' ')
+    
+    barcodes = barcodes_to_dict(BARCODES_PATH, sep='\t')
+    
+    # demultiplex(r1, r2, r3, r4, barcodes, counts)
+    counts = demultiplex(R1, R2, R3, R4, barcodes, BARCODE_PAIRS_PATH)
+    
+    # Saving output to a file so i can use to make heat map later
+    with open("counts.json", "w") as file:
+        json.dump(counts, file, indent=4)
     
